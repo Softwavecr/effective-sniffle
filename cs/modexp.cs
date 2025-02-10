12a7ae3adc78199b4fdae7913a2d5922620d6452
Jairo Guerrero <softwavecr@hotmail.com>
2025-02-09 21:26:00 -0500

    Code simplified

--- cs/modexp.cs
+++ cs/modexp.cs
@@ -2,39 +2,16 @@ static void Modulo(int divisor, int dividend)
 {
     int remainder = divisor % dividend;
     int trueModulo = TrueModulo(divisor,dividend);
-    bool correct = remainder == trueModulo;
-
-    string output = "";
-
-    if (divisor > -1) output += $" {divisor} MOD ";
-    else output += $"{divisor} MOD ";
-        
-    if (dividend > -1) output += $" {dividend}, OK? {(correct ? "Y" : "N")}.";        
-    else output += $"{dividend}, OK? {(correct ? "Y" : "N")}.";
-        
-    if (remainder > -1) output += $" R= {remainder} ,";        
-    else output += $" R={remainder} ,";
-
-    if (trueModulo > -1) output += $" T= {trueModulo}";
-    else output += $" T={trueModulo}";        
-
-    Console.WriteLine(output);
-}
-
-static void Modulo1(int divisor, int dividend)
-{
-    int remainder = divisor % dividend;
-    int trueModulo = TrueModulo(divisor,dividend);
     bool correct = remainder == trueModulo;     
 
-    Console.WriteLine($" {divisor} MOD {dividend}, OK? {(correct ? 'Y' : 'N')}. R= {remainder}, T= {trueModulo}.");
+    Console.WriteLine($" {divisor} MOD {dividend} = {(correct ? remainder : trueModulo)} (R = {remainder}, T = {trueModulo})");
 
 }
 
 static int TrueModulo(int divisor, int dividend) => 
     ((divisor % dividend) + dividend) % dividend;
 
-Modulo(10,3);Modulo1(10,3);
-Modulo(-10,3);Modulo1(-10,3);
-Modulo(10,-3);Modulo1(10,-3);
-Modulo(-10,-3);Modulo1(-10,-3);
+Modulo(10,3);
+Modulo(-10,3);
+Modulo(10,-3);
+Modulo(-10,-3);
--- js/modexp.js
+++ js/modexp.js
@@ -3,36 +3,14 @@ function modulo(divisor, dividend) {
     let trueModulo = trueMod(divisor, dividend);
     let correct = remainder === trueModulo;
 
-    let output = "";
-
-    if (divisor > -1) output += ` ${divisor} MOD `;
-    else output += `${divisor} MOD `;    
-
-    if (dividend > -1) output += ` ${dividend}, OK? ${correct ? "Y" : "N"}.`;
-    else output += `${dividend}, OK? ${correct ? "Y" : "N"}.`;
-
-    if (remainder > -1) output += ` R= ${remainder} ,`;
-    else output += ` R=${remainder} ,`;
-
-    if (trueModulo > -1) output += ` T= ${trueModulo}`;
-    else output += ` T=${trueModulo}`;
-
-    console.log(output);
+    console.log(` ${divisor} MOD ${dividend} = ${correct ? remainder : trueModulo} (R = ${remainder}, T = ${trueModulo})`);
 }
 
-function modulo1(divisor, dividend) {
-    let remainder = divisor % dividend;
-    let trueModulo = trueMod(divisor, dividend);
-    let correct = remainder === trueModulo;
+const trueMod = (divisor,dividend) => 
+    ((divisor % dividend) + dividend) % dividend;
 
-    console.log(` ${divisor} MOD ${dividend}, OK? ${correct ? 'Y' : 'N'}. R= ${remainder}, T= ${trueModulo}.`);
-}
 
-function trueMod(divisor,dividend) {
-    return ((divisor % dividend) + dividend) % dividend;
-}
-
-modulo(10, 3);modulo1(10, 3);
-modulo(-10, 3);modulo1(-10, 3);
-modulo(10, -3);modulo1(10, -3);
-modulo(-10, -3);modulo1(-10, -3);
+modulo(10, 3);
+modulo(-10, 3);
+modulo(10, -3);
+modulo(-10, -3);
