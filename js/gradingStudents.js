90af454b34392a7293cb43d5ca15bda490fff863
Jairo Guerrero <softwavecr@hotmail.com>
2025-02-11 00:20:48 -0500

    Clean up js

--- js/gradingStudents.js
+++ js/gradingStudents.js
@@ -1,14 +1,3 @@
-/*function gradingStudents(grades) {
-    let finalgrades = [];
-    grades.forEach(grade => {        
-        if(grade < 38 || grade % 5 == 0 || grade % 5 < 3)
-            finalgrades.push(grade);
-        else if(grade % 5 > 2)
-            finalgrades.push(grade + (5- (grade % 5)));
-    });
-    return finalgrades;
-}*/
-
 const gradeStudent = (grade) =>
     grade < 38 || grade % 5 == 0 || grade % 5 < 3 ? grade : 
     grade % 5 > 2 ? grade + (5- (grade % 5)):
