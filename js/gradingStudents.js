940b59bb2cc7e92e1cd25d99afc62463638adc03
Jairo Guerrero <softwavecr@hotmail.com>
2025-02-10 23:59:44 -0500

    functional

--- js/gradingStudents.js
+++ js/gradingStudents.js
@@ -1,13 +1,18 @@
-function gradingStudents(grades) {
+/*function gradingStudents(grades) {
     let finalgrades = [];
     grades.forEach(grade => {        
         if(grade < 38 || grade % 5 == 0 || grade % 5 < 3)
             finalgrades.push(grade);
         else if(grade % 5 > 2)
-            finalgrades.push(grade + (5- (grade % 5)));               
+            finalgrades.push(grade + (5- (grade % 5)));
     });
     return finalgrades;
-}
+}*/
 
-let fg = gradingStudents([73, 67, 38, 33]);
+const gradeStudent = (grade) =>
+    grade < 38 || grade % 5 == 0 || grade % 5 < 3 ? grade : 
+    grade % 5 > 2 ? grade + (5- (grade % 5)):
+    0;
+
+let fg = [73, 67, 38, 33].map(gradeStudent);
 console.log(fg);
