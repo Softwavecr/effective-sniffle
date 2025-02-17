7226d8221c4621abbb9671a1101f2620318b1f37
Jairo <jgl97@hotmail.com>
2025-02-16 19:51:34 -0500

    Squaresofintergers (#2)
    * ...
    * Square of first 25 integers

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # effective-sniffle
 Some practice in js, py and cs.
+
+Clean Architecture (book)
+Square of first 25 integers.
+
+Mutability
+Closure vs Java
---
+++ js/squareIntegers.js
@@ -0,0 +1,7 @@
+// Complete the minimumSwaps function below.
+function calculate() {
+let numbers = [...Array(25).keys()].map(i=>(i+1)*(i+1));
+console.log(numbers);
+}
+
+calculate();
